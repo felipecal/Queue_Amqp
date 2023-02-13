8658401601add07d7e48828d369762faba9d0244
Felipe <fcalldas@gmail.com>
2023-02-13 20:37:58 -0300

    ✨ introducing assertQueue and formating variable

--- src/receive.js
+++ src/receive.js
@@ -4,29 +4,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const exchange = 'account';
+const queue = 'account-queue';
 
 (async () => {
   try {
     const connection = await amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`);
     const channel = await connection.createChannel();
-
-    process.once('SIGINT', async () => { 
-      await channel.close();
-      await connection.close();
-    });
-
     await channel.assertExchange(exchange, 'fanout', { durable: false });
-    const { queue } = await channel.assertQueue('', { exclusive: true });
+    await channel.assertQueue(queue, { exclusive: true });
     await channel.bindQueue(queue, exchange, '')
-
     await channel.consume(queue, (message) => {
       const msg = message.content.toString()
       if (message) console.log( JSON.parse(msg));
-      else console.warn(' [x] Consumer cancelled');
     }, { noAck: true });
 
-    console.log(' [*] Waiting for logs. To exit press CTRL+C');
-  } catch (err) {
-    console.warn(err);
+    console.log(' 🚀 🚀 [*] Waiting for logs 🚀 🚀 .');
+  } catch (error) {
+    throw new Error(error);
   }
 })();
--- src/sender.js
+++ src/sender.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const queue = 'account-queue';
 const exchange = 'account';
 const text = {
   name: 'Caldas',
@@ -16,7 +17,7 @@ const text = {
     connection = await amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`);
     const channel = await connection.createChannel();
     await channel.assertExchange(exchange, 'fanout', { durable: false });
-    channel.publish(exchange, '', Buffer.from(JSON.stringify(text)));
+    channel.publish(exchange, queue, Buffer.from(JSON.stringify(text)));
     console.log(text);
     await channel.close();
   }
