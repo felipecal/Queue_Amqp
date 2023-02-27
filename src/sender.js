5e934eafd62088cf7842009d6798c15453cf855b
Felipe <fcalldas@gmail.com>
2023-02-27 10:56:29 -0300

    ✨ new features about publish in 2 queues

--- src/sender.js
+++ src/sender.js
@@ -6,13 +6,22 @@ dotenv.config();
 let connection;
 let channel;
 
-const queue = 'account-queue';
+const userKey = 'userKey';
+const productKey= 'productKey';
+
 const exchange = 'account';
-const text = {
+
+const userMessage = {
   name: 'Caldas',
   age: 20, 
   state: 'Brazil',
   birthdate: new Date()
+};
+
+const productMessage = {
+  name: 'RTX 3090 GALAX',
+  price: 3900.00, 
+  quantity: 5
 };
 
 (async () => {
@@ -20,9 +29,12 @@ const text = {
   try {
     connection = await amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`);
     channel = await connection.createChannel();
-    await channel.assertExchange(exchange, 'fanout', { durable: false });
-    channel.publish(exchange, queue, Buffer.from(JSON.stringify(text)));
-    console.log(text);
+    await channel.assertExchange(exchange, 'topic', { durable: false });
+    await channel.publish(exchange, userKey, Buffer.from(JSON.stringify(userMessage)));
+    channel.publish(exchange, productKey, Buffer.from(JSON.stringify(productMessage)));
+    console.log('userMessage\n:', userMessage);
+    console.log();
+    console.log('productMessage\n:', productMessage);
     await channel.close();
   }
   catch (error) {
